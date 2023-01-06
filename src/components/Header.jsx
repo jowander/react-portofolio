3b4920f64774fc005c2d590d3f47d4be47f322f9
Willy Andersen <jowander_@hotmail.com>
2023-01-06 20:07:26 +0100

    tailwind installed

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-center text-2xl bg-yellow-500 text-black p-4">
+                Portofolio
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
